dda20c9038f45c9c2641b10afd2dc6c8dbbab40b
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2026-09-20 18:57:08 -0300

    Score

---
+++ Assets/Script/Game/Enemy/EnemyScoreAllocator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyScoreAllocator : MonoBehaviour
+{
+    [SerializeField]
+    private int _killScore;
+
+    private ScoreController _scoreController;
+
+    private void Awake()
+    {
+        _scoreController = FindObjectOfType<ScoreController>();
+    }
+
+    public void AllocateScore()
+    {
+        _scoreController.AddScore(_killScore);
+    }
+
+}

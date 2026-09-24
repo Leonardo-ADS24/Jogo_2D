7de52a7ab60e99f3ced59f76de2c8fbdc59e71a6
Leonardo-ADS24 <leonardo.santiago@alunos.unis.edu.br>
2026-09-23 21:54:54 -0300

    Sprite Flash

--- Assets/Script/Game/Health/InvincibilityController.cs
+++ Assets/Script/Game/Health/InvincibilityController.cs
@@ -6,21 +6,23 @@ public class InvicibillityController : MonoBehaviour
 {
 
     private HealthController _healthController;
+    private SpriteFlash _spriteFlash;
 
     private void Awake()
     {
         _healthController = GetComponent<HealthController>();
+        _spriteFlash = GetComponent<SpriteFlash>();
     }
 
-    public void StartInvincibility(float invincibilityDuration)
+    public void StartInvincibility(float invincibilityDuration, Color flashColor, int numberOfFlashes)
     {
-        StartCoroutine(InvinvicilityCoroutine(invincibilityDuration));
+        StartCoroutine(InvinvicilityCoroutine(invincibilityDuration, flashColor, numberOfFlashes));
     }
 
-    private IEnumerator InvinvicilityCoroutine(float invincibilityDuration)
+    private IEnumerator InvinvicilityCoroutine(float invincibilityDuration, Color flashColor, int numberOfFlashes)
     {
         _healthController.isInvincible = true;
-        yield return new WaitForSeconds(invincibilityDuration);
+        yield return _spriteFlash.FlashCoroutine(invincibilityDuration,flashColor, numberOfFlashes);
         _healthController.isInvincible = false;
     }    
 
